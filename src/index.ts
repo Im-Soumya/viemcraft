942ba5e465047184093bdc506341e088a96fe876
Im-Soumya <soumyamukherjee.work@gmail>
2024-12-04 15:13:11 +0530

    add: generateCallData fn

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+import { encodeFunctionData } from 'viem';
+import { GenerateCalldataParams } from './types';
+
 export function greet(name: string): string {
-  return `Hello, ${name}!`;
+  return `${name}, welcome to viemcraft 🧶`;
+}
+
+export function generateCalldata({
+  abi,
+  functionName,
+  args,
+}: GenerateCalldataParams) {
+  return encodeFunctionData({
+    abi,
+    functionName,
+    args,
+  });
 }
